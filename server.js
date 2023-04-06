c7c4328567f9d16a502651fbe70e06f1f641c870
jrob02180 <jrob02180@gmail.com>
2023-04-05 21:23:06 -0600

    Got married to back end

--- server.js
+++ server.js
@@ -17,24 +17,6 @@ const PORT = process.env.PORT || 3001;
 // // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {
-//     maxAge: 300000,
-//     httpOnly: true,
-//     secure: false,
-//     sameSite: 'strict',
-//   },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-
-// app.use(session(sess));
-
 const sess = {
   secret: 'Super secret secret',
   cookie: {
