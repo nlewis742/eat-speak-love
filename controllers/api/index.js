081dff3416be4e22b0bed42a508214d0af53c190
Natasha <nlackas12@gmail.com>
2023-04-05 20:45:49 -0500

    have an html page that displays

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
-// const router = require('express').Router();
-// const userRoutes = require('./userRoutes');
-// const projectRoutes = require('./postRoutes');
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
 
-// router.use('/users', userRoutes);
-// router.use('/projects', postRoutes);
+router.use('/users', userRoutes);
+router.use('/post', postRoutes);
 
-// module.exports = router;
+module.exports = router;
