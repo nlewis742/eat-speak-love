7524784ee5d8ee9301b7169ef3ecc745db9e2689
mrott0397 <119753667+mrott0397@users.noreply.github.com>
2023-04-05 20:40:27 -0600

    copied mini project, created mockViews

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,2 @@
--- DROP DATABASE IF EXISTS crowdfund_db;
--- CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS forum_db;
+CREATE DATABASE forum_db;
--- server.js
+++ server.js
@@ -31,16 +31,16 @@ const hbs = exphbs.create({ helpers });
 
 // app.use(session(sess));
 
-// // Inform Express.js on which template engine to use
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
