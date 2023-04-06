f5c43ee5197913ab88dc0782ed7c099b4114970d
Natasha <nlackas12@gmail.com>
2023-04-05 20:32:11 -0500

    populating backend files

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
-// const User = require('./User');
-// const Project = require('./Project');
+const User = require('./User');
+const Post = require('./Post');
 
-// User.hasMany(Project, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
-// Project.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-// module.exports = { User, Project };
+module.exports = { User, Post };
