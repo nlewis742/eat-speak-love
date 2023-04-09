2e17cf5643b849c5f6da2cf0935b210f8d36ed50
Natasha <nlackas12@gmail.com>
2023-04-09 10:09:04 -0500

    added effect on image

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,18 +2,21 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+
 router.post('/', withAuth, async (req, res) => {
   try {
+    
     const newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-
+    
     res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
   console.log("post");
+ 
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
